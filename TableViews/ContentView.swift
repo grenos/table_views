d3d9dd00c6ab203b68a88f5af9e988e82b60ca93
grenos <grenos>
2020-02-22 13:45:12 +0100

    Initial Commit

---
+++ TableViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableViews
+//
+//  Created by Vasileios Gkreen on 22/02/2020.
+//  Copyright © 2020 Vasileios Gkreen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
